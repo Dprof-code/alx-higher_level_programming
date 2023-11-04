4d01d4b1649e5a141bf35da9eb9a019a3d6305e2
dprof-code <olawaleade15@gmail.com>
2023-11-03 22:33:20 -0700

    fixed all

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(99):
-    print(f"{str(0) + str(i) if i < 10 else i}", end=", ")
+    print("{:02}".format(i), end=", ")
 print(99)
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(str):
     for char in str:
         if (ord(char) >= 97 and ord(char) <= 122):
             upper_code = 90 - (122 - ord(char))
-            print(f"{chr(upper_code)}", end="")
+            print("{}".format(chr(upper_code)), end="")
         else:
-            print(f"{char}", end="")
+            print("{}".format(char), end="")
     print("")
