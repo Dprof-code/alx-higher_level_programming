4cb3a8ee9c162874fe6e95463cae36651e39c3e3
dprof-code <olawaleade15@gmail.com>
2023-11-04 17:19:47 -0700

    completed tasks in 0x02. Python - import & modules project

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import hidden_v
+    import hidden_4
 
-    dir(hidden_v)
+    for name in dir(hidden_4):
+        if name[0] != '_':
+            print(name)
