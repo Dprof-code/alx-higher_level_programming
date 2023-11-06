3ffac9893c5ab85b435d8479dd354977b8d7eb9a
dprof-code <olawaleade15@gmail.com>
2023-11-06 15:23:28 -0800

    fixed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,6 @@ def print_matrix_integer(matrix=[[]]):
     for set_of_int in matrix:
         i = 0
         while i < len(set_of_int):
-            print("{}".format(set_of_int[i]), end=" ")
+            print("{:d}".format(set_of_int[i]), end=" ")
             i += 1
         print()
