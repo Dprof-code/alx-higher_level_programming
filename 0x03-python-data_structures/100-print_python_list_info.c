7c9e80d86c9d6c294aa93f1b0b54928a67b6bef7
dprof-code <olawaleade15@gmail.com>
2023-11-07 20:28:15 -0800

    advanced task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_python_list_info -  prints some basic info about Python lists.
+ *
+ * @p: Python list
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t list_size = PyList_Size(p);
+
+	if (PyList_Check(p))
+	{
+		printf("[*] Size of the Python List = %ld\n", list_size);
+		printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+		for (Py_ssize_t i = 0; i < list_size; i++)
+		{
+			PyObject *item = PyList_GetItem(p, i);
+			printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+		}
+	}
+	else
+	{
+		printf("  [ERROR] Invalid List Object\n");
+	}
+}
---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,11 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p);
+
+#endif
