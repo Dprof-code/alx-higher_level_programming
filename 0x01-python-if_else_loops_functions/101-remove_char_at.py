7065ef0bf62f3c05c1fd268a155eab49c03970f3
Dprof-Code <olawaleade15@gmail.com>
2023-11-30 06:09:50 +0100

    completed tasks

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def remove_char_at(str, n):
+    """
+    remove_char_at - creates a copy of the string,
+    removing the character at the position n
+    """
+    if n < 0:
+        return str
+    return str[:n] + str[n + 1:]
